f77b781ddfcf88d9f717e05233002b8ec2b7bc36
Anand Raj <anandrajj10@gmail.com>
2026-07-26 08:23:26 +0530

    traffic

--- TrafficLight/app.js
+++ TrafficLight/app.js
@@ -1,13 +1,13 @@
-// Create Traffic Light
+// Traffic Light
 console.log("Traffic Light Loaded!");
 
 const lights = document.querySelectorAll(".circle");
-const durations = [4000, 500, 3000];
+const durations = [3000, 1000, 3000];
 let currentLight = 0;
 
 function colorChange() {
-  lights.forEach((lights, index) => {
-    lights.classList.toggle("active", index === currentLight);
+  lights.forEach((light, index) => {
+    light.classList.toggle("active", index === currentLight);
   });
 
   setTimeout(() => {
@@ -15,6 +15,7 @@ function colorChange() {
     colorChange();
   }, durations[currentLight]);
 }
+
 colorChange();
 
-//https://blog.stackademic.com/building-a-traffic-light-system-a-fun-take-on-frontend-interviews-9b3ae6d876b7
+// https://blog.stackademic.com/building-a-traffic-light-system-a-fun-take-on-frontend-interviews-9b3ae6d876b7
